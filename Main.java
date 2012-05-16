eaa6a35acad64a73f9b23f933f4387a7c8630e32
zhanhx <zhanhaoxiang@gmail.com>
2012-05-16 17:23:46 +0800

    Test Add File

---
+++ Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kusoft.im;
+
+import com.kusoft.im.net.Connection;
+
+/**
+ *
+ * @author zhanhx
+ */
+public class Main {
+    public static void main(String [] args) {
+        new Connection.Daemon(80).start();
+    }
+    
+}
