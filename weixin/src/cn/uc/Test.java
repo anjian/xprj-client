39bffe6d679f783337a2c91e04bb9d63678e036f
yangxing <yangxing>
2012-06-30 14:33:58 +0800

    添加一个类，测试push

---
+++ weixin/src/cn/uc/Test.java
@@ -0,0 +1,31 @@
+/**
+ * Test.java
+ * 
+ * Copyright (c) 2004-2010 UC Mobile Ltd.
+ * F/4,Building B,Guangzhou Xinxigang,No.16 Keyun Rd,Guangzhou,China
+ * All right reserved.
+ * 
+ * Created on 2012-6-30
+ */
+package cn.uc;
+
+/**
+ * //TODO Administrator自动生成javadoc
+ * <b>Test简介:</b>
+ * <p>简要的描述此类的作用</p>
+ *
+ * <b>功能描述:</b>
+ * <p>具体功能点描述，当添加新功能时，要求更新此描述</p>
+ *
+ * <b>修改历史</b>
+ * <p>
+ *  <ol>
+ *   <li>创建（Added by Administrator on 2012-6-30）</li>
+ *  </ol> 
+ * </p>
+ * @author Administrator
+ * @version 1.0
+ */
+public class Test {
+
+}
